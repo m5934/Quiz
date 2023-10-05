41bf4d16baeddd02cb2e40f49d9ed94f42995b4e
Michał <89794322+m5934@users.noreply.github.com>
2023-10-05 19:38:50 +0000

    Dodanie_kodu_bazy_danych

---
+++ QUIZZ.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Pytania
+	(NR_PYTANIA SERIAL PRIMARY KEY,
+	 PYTANIE CHARACTER VARYING(100),
+	ODP_1 CHARACTER VARYING(100),
+	ODP_2 CHARACTER VARYING(100),
+	ODP_3 CHARACTER VARYING(100),
+	ODP_4 CHARACTER VARYING(100),
+	NR_POP_ODP NUMERIC(1));
+	
+
+INSERT INTO Pytania (PYTANIE,ODP_1,ODP_2,ODP_3, ODP_4, NR_POP_ODP)
+VALUES ( 'What is the capital of Australia','Canberra', 'Sydney', 'Hobart', NULL, 2);
+
+INSERT INTO Pytania (PYTANIE,ODP_1,ODP_2,ODP_3, ODP_4, NR_POP_ODP)
+VALUES ( 'What is the capital of Japan', 'Tokyo', 'Hiroshima', 'Osaka', 'Kyoto', 1);
